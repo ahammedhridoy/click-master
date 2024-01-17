965475290df114a6128b33ca7a03aa37e64d6004
devlopersabbir <devlopersabbir@gmail.com>
2024-01-18 01:37:10 +0600

    [style 🎭] Simple popup UI

--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -1,14 +1,64 @@
-import React, { useEffect } from "react";
-import "./Popup.css";
+import { ChangeEvent, useEffect, useState } from "react";
+import "./popup.css";
+import icon from "/icon.png";
+import browser from "webextension-polyfill";
 
 export default function () {
+  const [version, setVersion] = useState<string>("");
+  const [name, setName] = useState<string>("");
+
   useEffect(() => {
-    console.log("Hello from the popup!");
-  }, []);
+    const { version, name } = browser.runtime.getManifest();
+    setVersion(version);
+    setName(name);
+  });
 
   return (
-    <React.Fragment>
-      <h1>Hello World</h1>
-    </React.Fragment>
+    <div className="w-[400px] h-full bg-gray-900 text-white p-2">
+      <div className="flex justify-center items-center flex-col gap-2 mb-5">
+        <img src={icon} alt="logo" className="w-14" />
+        <h1 className="text-2xl font-semibold">{name}</h1>
+      </div>
+      <div className="flex flex-col gap-4 justify-start items-center m-2 p-4 rounded-xl shadow-2xl bg-gray-800 backdrop-blur-3xl backdrop:blur-3xl">
+        <div className="flex w-full justify-between items-center text-lg">
+          <label htmlFor="maxTiket" className="font-mono">
+            Select Max ticket
+          </label>
+          <select
+            id="maxTiket"
+            className="w-32 bg-gray-900 text-white border-2 border-gray-400 py-0.5 px-4 text-center"
+            defaultValue={6}
+          >
+            <option>Select</option>
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
+              <option
+                onChange={(e: ChangeEvent<HTMLOptionElement>) =>
+                  console.log("value", e.target.value)
+                }
+                value={item}
+                key={index}
+              >
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex w-full justify-between items-center text-lg">
+          <label
+            htmlFor=""
+            className="font-mono relative inline-flex items-center cursor-pointer"
+          >
+            Real static
+          </label>
+          <label className="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" />
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </label>
+        </div>
+      </div>
+      <p className="text-center text-gray-400 my-2">
+        Version: <strong>{version}</strong>
+      </p>
+    </div>
   );
 }
--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,20 +1,7 @@
 import browser from "webextension-polyfill";
-import { on, off, fire } from "delegated-events";
-import * as cheerio from "cheerio";
 
-let isClickListening = false;
-
-// // browser.runtime.
-// data-seat-name
-on(
-  "click",
-  ".seatstyles__Seat-sc-1efhiok-0",
-  (event: MouseEvent) => {
-    const $ = cheerio.load(`${event.target}`);
-    console.log("element: ", $(".seatstyles__Seat-sc-1efhiok-0").html());
-    // $("")
-  },
-  {
-    capture: true,
-  }
-);
+browser.runtime.onMessage.addListener((mess, sender, sendRes) => {
+  console.log("mess", mess);
+  console.log("sender", sender);
+  sendRes();
+});
